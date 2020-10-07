eb303900309e166fb9be10bdc41e491115640962
reirei62 <rei1gggg@gmail.com>
2020-10-08 02:49:18 +0900

    boardがないユーザーに飛ばないようにリンクを修正

--- app/controllers/introductions_controller.rb
+++ app/controllers/introductions_controller.rb
@@ -1,4 +1,5 @@
 class IntroductionsController < ApplicationController
+  before_action :go_login,only: [:create,:new]
   before_action :set_board_and_user, only: [:create,:new]
   before_action :set_introduction, only: [:destroy,:change]
   before_action :back_defferent_person, only: [:destroy]
@@ -9,7 +10,6 @@ class IntroductionsController < ApplicationController
   end
 
   def create
-    authenticate_user!
     @introduction=Introduction.new(introduction_params)
     if @introduction.save!
       redirect_to board_path(id: @user[0][:id])
@@ -34,30 +34,33 @@ class IntroductionsController < ApplicationController
 
 
   private
-
-  def introduction_params
-    params.permit(:content,:category_id).merge(boards_id: params[:board_id],users_id: current_user.id)
+  def go_login
+    authenticate_user!
   end
-
+  
   def set_board_and_user
     @board=Board.find(params[:board_id])
     @user=User.where(id: @board.users_id)
   end
-
+  
   def set_introduction
     @introduction=Introduction.find(params[:board_id])
   end
-
+  
   def back_defferent_person
     unless current_user.id == params[:id] || @introduction.users_id == current_user.id
       redirect_to root_path
     end
   end
-
+  
   def back_not_current_user
     board=Board.where(id: @introduction.boards_id)
     unless current_user.id == board[0][:users_id]
       redirect_to root_path
     end
+  end
+
+  def introduction_params
+    params.permit(:content,:category_id).merge(boards_id: params[:board_id],users_id: current_user.id)
   end
 end
